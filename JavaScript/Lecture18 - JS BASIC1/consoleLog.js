50788a167372abea88442e9c82ee1547cd160f93
My website <nitish.edu.in@gmail.com>
2023-09-02 12:01:52 +0530

    JavaScript

---
+++ JavaScript/Lecture18 - JS BASIC1/consoleLog.js
@@ -0,0 +1,25 @@
+console.log("My Name is Nitish Kumar");
+let name1 = "Nitish kumar"
+{
+   let name1 = "aryan"
+   console.log(name1);
+}
+console.log(name1);
+
+
+let name = "Nitish kumar"
+{
+    name = "aryan"
+    console.log(name);
+} 
+console.log(name);
+
+
+var nam = "nitish"
+{
+    var nam = "Rana"
+    console.log(nam)
+    
+}
+console.log(nam)
+

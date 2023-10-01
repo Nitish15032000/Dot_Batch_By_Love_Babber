401de112c41ef36b2b3478bdd9f44c4a3a4fea5d
My website <nitish.edu.in@gmail.com>
2023-10-01 18:19:50 +0530

    addEventlistener properties

--- JavaScript/Lecture23 - BOM1/index.js
+++ JavaScript/Lecture23 - BOM1/index.js
@@ -1,6 +1,7 @@
-// monitorEvents(document);
+// // monitorEvents(document);
 
-// element.addEventListener('click',function(){element.style.color = 'red'})
+// // element.addEventListener('click',function(){
+// //     element.style.color = 'red'});
 
 // document.addEventListener('click',function(){
 //     console.log("I have clicked");
@@ -16,25 +17,25 @@
 // });
 
 
-// let myDiv = document.createElement('div');
+let myDiv = document.createElement('div');
 
-// function paraStatus(event) {
-//     console.log('Para ' + event.target.textContent);
-// }
-// myDiv.addEventListener('click', paraStatus);
+function paraStatus(event) {
+    console.log('Para ' + event.target.textContent);
+}
+myDiv.addEventListener('click', paraStatus);
 
-// for(let i=1; i<=100; i++) {
-//     let newElement = document.createElement('p');
-//     newElement.textContent = 'This is para ' + i;
+for(let i=1; i<=100; i++) {
+    let newElement = document.createElement('p');
+    newElement.textContent = 'This is para ' + i;
 
-//     myDiv.appendChild(newElement);
-// }
-// document.body.appendChild(myDiv);
+    myDiv.appendChild(newElement);
+}
+document.body.appendChild(myDiv);
 
-let element = document.querySelector('#wrapper');
+// let element = document.querySelector('#wrapper');
 
-element.addEventListener('click', function(event) {
-    if(event.target.nodeName === 'SPAN') {
-        console.log('span pr click kia hai' + event.target.textContent);
-    }
-});
+// element.addEventListener('click', function(event) {
+//     if(event.target.nodeName === 'SPAN') {
+//         console.log('span pr click kia hai' + event.target.textContent);
+//     }
+// });
